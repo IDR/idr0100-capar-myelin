825bad9b1b3808bf1474ea583907bc38f80b9b53
Dominik Lindner <d.lindner@dundee.ac.uk>
2020-11-09 15:14:08 +0000

    Use relative paths and create pattern files

--- scripts/generate_pattern_files.sh
+++ scripts/generate_pattern_files.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Axon.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample1_C1.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Ground_Truth.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample1_C4.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Nucleus.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample1_C2.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Oligodendrocyte.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample1_C3.tif
-echo "Sample1_C<1-4>.tif" > "/uod/idr/metadata/idr0100-capar-myelin/pattern/Sample1.pattern"
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Axon.tif ../pattern/Sample1_C1.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Ground_Truth.tif ../pattern/Sample1_C4.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Nucleus.tif ../pattern/Sample1_C2.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images1/Sample1_Oligodendrocyte.tif ../pattern/Sample1_C3.tif
+echo "Sample1_C<1-4>.tif" > "../pattern/Sample1.pattern"
 
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Axon.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample2_C1.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Ground_Truth.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample2_C4.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Nucleus.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample2_C2.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Oligodendrocyte.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample2_C3.tif
-echo "Sample2_C<1-4>.tif" > "/uod/idr/metadata/idr0100-capar-myelin/pattern/Sample2.pattern"
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Axon.tif ../pattern/Sample2_C1.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Ground_Truth.tif ../pattern/Sample2_C4.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Nucleus.tif ../pattern/Sample2_C2.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images2/Sample2_Oligodendrocyte.tif ../pattern/Sample2_C3.tif
+echo "Sample2_C<1-4>.tif" > "../pattern/Sample2.pattern"
 
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Axon.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample3_C1.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Ground_Truth.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample3_C4.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Nucleus.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample3_C2.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Oligodendrocyte.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample3_C3.tif
-echo "Sample3_C<1-4>.tif" > "/uod/idr/metadata/idr0100-capar-myelin/pattern/Sample3.pattern"
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Axon.tif ../pattern/Sample3_C1.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Ground_Truth.tif ../pattern/Sample3_C4.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Nucleus.tif ../pattern/Sample3_C2.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images3/Sample3_Oligodendrocyte.tif ../pattern/Sample3_C3.tif
+echo "Sample3_C<1-4>.tif" > "../pattern/Sample3.pattern"
 
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Axon.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample4_C1.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Ground_Truth.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample4_C4.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Nucleus.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample4_C2.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Oligodendrocyte.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample4_C3.tif
-echo "Sample4_C<1-4>.tif" > "/uod/idr/metadata/idr0100-capar-myelin/pattern/Sample4.pattern"
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Axon.tif ../pattern/Sample4_C1.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Ground_Truth.tif ../pattern/Sample4_C4.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Nucleus.tif ../pattern/Sample4_C2.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images4/Sample4_Oligodendrocyte.tif ../pattern/Sample4_C3.tif
+echo "Sample4_C<1-4>.tif" > "../pattern/Sample4.pattern"
 
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Axon.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample5_C1.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Ground_Truth.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample5_C4.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Nucleus.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample5_C2.tif
-ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Oligodendrocyte.tif /uod/idr/metadata/idr0100-capar-myelin/pattern/Sample5_C3.tif
-echo "Sample5_C<1-4>.tif" > "/uod/idr/metadata/idr0100-capar-myelin/pattern/Sample5.pattern"
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Axon.tif ../pattern/Sample5_C1.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Ground_Truth.tif ../pattern/Sample5_C4.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Nucleus.tif ../pattern/Sample5_C2.tif
+ln -s /uod/idr/filesets/idr0100-capar-myelin/20201022-ftp/experimentA/Sample_Myelin_Images5/Sample5_Oligodendrocyte.tif ../pattern/Sample5_C3.tif
+echo "Sample5_C<1-4>.tif" > "../pattern/Sample5.pattern"
